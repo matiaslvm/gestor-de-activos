1f863a2b6df2afaeb0b85c3e514e707e708149b1
matiaslvm <abazcu04@gmail.com>
2024-11-15 11:56:11 -0300

    dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -62,7 +62,9 @@ function App() {
               <Route
                 path="/"
                 element={
-                  <Dashboard assets={[]}/>
+                  <><div className="px-4 sm:px-6 lg:px-8">
+                    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
+                  </div><Dashboard assets={[]} /></>
                 }
               />
               <Route
