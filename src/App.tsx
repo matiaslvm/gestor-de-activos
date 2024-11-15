6f87373fac06e2d260ecdb8f56eac204da5747a0
matiaslvm <abazcu04@gmail.com>
2024-11-15 12:00:01 -0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -62,9 +62,7 @@ function App() {
               <Route
                 path="/"
                 element={
-                  <><div className="px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-2xl font-semibold text-gray-900">Dashboard</h1>
-                  </div><Dashboard assets={[]} /></>
+                  <Dashboard assets={[]}/>
                 }
               />
               <Route
